3163094a52a9a11afaa305e1e3336318467ebf2a
iasonas <iasonaspav@gmail.com>
2018-10-30 12:57:45 +0200

    Script for results

--- Exc1/scripts.sh
+++ Exc1/scripts.sh
@@ -11,10 +11,15 @@ do
             for z in {0..2}
             do
                 printf "%s\t\t" "Input size:" $i >> results.txt
-                ./$j $i $x >> results.txt
+                ./$j $i $x 2 >> results.txt
                 printf "\n" >> results.txt
             done
         done
         #echo "---------------------------------------------" >> results.txt
     done
 done
+
+for j in {12..24}
+do
+    printf "%s\t\t" "Input size:" $i >> results.txt
+    ./qsort $j 2
