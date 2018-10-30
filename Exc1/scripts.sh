c320331e0911582c39e04f4443ea8a6b91181d01
iasonas <iasonaspav@gmail.com>
2018-10-30 13:01:21 +0200

    Script for results

--- Exc1/scripts.sh
+++ Exc1/scripts.sh
@@ -2,24 +2,34 @@
 
 for j in *.out
 do
-    echo "$j results:" >> results.txt
-    echo "==========================================" >> results.txt
+    echo "$j results:" >> results_$j
+    echo "==========================================" >> results_$j
     for i in {12..24}
     do
         for x in {0..8}
         do
-            for z in {0..2}
+            if [ "$i" == "24" ]
+            then
+                end=2
+            else 
+                end=0
+            fi
+            for z in $(seq 0 $end)
             do
-                printf "%s\t\t" "Input size:" $i >> results.txt
-                ./$j $i $x 2 >> results.txt
-                printf "\n" >> results.txt
+                printf "%s\t\t" "Input size:" $i >> results_$j
+                ./$j $i $x 2 >> results_$j
+                printf "\n" >> results_$j
             done
         done
-        #echo "---------------------------------------------" >> results.txt
-    done
+     done
 done
+
+echo "Sequential results:" >> results.txt
+echo "==========================================" >> results.txt
 
 for j in {12..24}
 do
-    printf "%s\t\t" "Input size:" $i >> results.txt
-    ./qsort $j 2
+    printf "%s\t\t" "Input size:" $j >> results.txt
+    ./qsort $j 2 >> results.txt
+    printf "\n" >> results.txt
+done
