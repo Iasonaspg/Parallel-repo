1346a019b83a11e3ab4f3a54647cc914f94f4b98
iasonas <iasonaspav@gmail.com>
2019-01-29 18:46:53 +0200

    Added comments

--- Exc3/cudaKNN.cu
+++ Exc3/cudaKNN.cu
@@ -16,6 +16,7 @@
     } \
 } \
 
+// Kernel to change 1 with 0 because we want values inside [0,1) and currand produces (0,1]
 __global__ void corr(float* ptr, size_t N){
 
     int index = threadIdx.x + blockDim.x*blockIdx.x;
@@ -27,6 +28,7 @@ __global__ void corr(float* ptr, size_t N){
     }
 }
 
+// Kernel to print points from a big array
 __global__ void check(float* ptr, size_t N, int len){
 
     int index = threadIdx.x + blockDim.x*blockIdx.x;
@@ -38,6 +40,7 @@ __global__ void check(float* ptr, size_t N, int len){
 
 }
 
+// Kernel to initialize a big array with a specific number. I eventually didn't use it
 __global__ void init(float* ptr, int len, int num){
 
     int index = threadIdx.x + blockDim.x*blockIdx.x;
@@ -48,6 +51,7 @@ __global__ void init(float* ptr, int len, int num){
     }
 }
 
+// Kernel to find the nearest neighbor of a point
 __global__ void findNN(float* queries, int len, float* boxesC, int len1, int elements, float side, int* aux, int d, float* distances){
 
     int index = threadIdx.x + blockDim.x*blockIdx.x;
@@ -61,13 +65,13 @@ __global__ void findNN(float* queries, int len, float* boxesC, int len1, int ele
         coX = queries[i]/side;
         coY = queries[i+len]/side;
         coZ = queries[i+2*len]/side;
-        int box = coX + coY*d + coZ*d*d;
+        int box = coX + coY*d + coZ*d*d;    // calculate the box in which the point belongs to
         // printf("Box inside kernel: %d\n",box);
         // printf("Queries inside kernel: (%f,%f,%f)\n",queries[i],queries[i+len],queries[i+2*len]);
         float min = 10000000;
         int pos = -1;
         boxMin = -1;
-        start = box*elements;
+        start = box*elements;   // Starting point inside the big 1-d array for the desired box
         for (int j=0; j<aux[box];j++){
             sum = 0;
             for (int k=0; k<3; k++){
@@ -76,12 +80,13 @@ __global__ void findNN(float* queries, int len, float* boxesC, int len1, int ele
             distance = powf(sum,0.5);
             //printf("Distance: %f\n", distance);
             if (distance < min){
-                min = distance;
+                min = distance;     // Calculate the min distance inside the box
                 pos = j;
                 boxMin = box;
             }
         }
 
+        // Check if I have to search adjacent boxes
         if ((fabsf(queries[i+2*len]-(coZ+1)*side)<min) || (fabsf(queries[i+2*len]-coZ*side)<min) || (fabsf(queries[i+len]-(coY+1)*side)<min) || (fabsf(queries[i+len]-coY*side)<min) || (fabsf(queries[i]-(coX+1)*side)<min) || (fabsf(queries[i]-coX*side)<min)){
             for (int dx=-1;dx<=1;dx++){
                 coXtmp = coX + dx;
@@ -124,10 +129,11 @@ __global__ void findNN(float* queries, int len, float* boxesC, int len1, int ele
         // printf("Min distance from (%f,%f,%f): %f at box: %d and pos: %d\n",queries[i],queries[i+len],queries[i+2*len],min,boxMin,pos);
         int start1 = boxMin*elements;
         // printf("Nearest neighbor of (%f,%f,%f): (%f,%f,%f)\n",queries[i],queries[i+len],queries[i+2*len],boxesC[start1+pos],boxesC[start1+pos+len1],boxesC[start1+pos+2*len1]);
-        distances[i] = min;
+        distances[i] = min;  // Store the final min distance
     }
 }
 
+// Kernel that calculates minimum distance in a brute-force way
 __global__ void validate(float* queries, float* corpus, int len, int* flag, float* distances){
 
     int index = threadIdx.x + blockDim.x*blockIdx.x;
@@ -153,6 +159,7 @@ __global__ void validate(float* queries, float* corpus, int len, int* flag, floa
 
 }
 
+// Function that returns current time in seconds
 double cpuSecond() {
     struct timeval tp;
     gettimeofday(&tp,NULL);
@@ -163,23 +170,26 @@ double cpuSecond() {
 
 int main(int argc, char** argv){
 
-    size_t Nq = (1<<atoi(argv[1]));
+    size_t Nq = (1<<atoi(argv[1]));     // Nq=Nc
     int d = (1<<atoi(argv[2]));
     float side = 1/((float) d);
     printf("Side length of each box: %f\n",side);
+    
+    
     curandGenerator_t gen;
     float *queries, *corpus;
 
+    // Generate Uniform Points
     CHECK(cudaMallocManaged(&queries, 3*Nq*sizeof(float)));
     CHECK(cudaMallocManaged(&corpus, 3*Nq*sizeof(float)));
     curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
     curandSetPseudoRandomGeneratorSeed(gen,time(NULL));
+    
+    double start = cpuSecond();
     curandGenerateUniform(gen, queries, 3*Nq);
     curandGenerateUniform(gen, corpus, 3*Nq);
-
-    double start = cpuSecond();
-    corr<<<6144,1024>>>(queries,3*Nq);
-    corr<<<6144,1024>>>(corpus,3*Nq);
+    corr<<<((Nq+1023)/1024),1024>>>(queries,3*Nq);
+    corr<<<((Nq+1023)/1024),1024>>>(corpus,3*Nq);
     CHECK(cudaDeviceSynchronize());
     double elaps = cpuSecond() - start;
     printf("Time elapsed for generating points: %f seconds\n",elaps);
@@ -189,14 +199,14 @@ int main(int argc, char** argv){
 
     int newNq = 8*Nq;
     float* boxesC;
-    CHECK(cudaMallocManaged(&boxesC, 3*newNq*sizeof(float)));
+    CHECK(cudaMallocManaged(&boxesC, 3*newNq*sizeof(float)));   // Array that is going to store the points
     
-    int* auxC;
+    int* auxC;                                                  // Array that will store the number of points inside each box
     CHECK(cudaMallocManaged(&auxC, d*d*d*sizeof(int)));
     CHECK(cudaMemset(auxC,0,d*d*d*sizeof(int)));
     
 
-    int elements = newNq/(d*d*d);
+    int elements = newNq/(d*d*d);       // Number of points that a box can contain
     printf("Elements: %d\n",elements);
 
     start = cpuSecond();
@@ -205,14 +215,14 @@ int main(int argc, char** argv){
         coX_C = corpus[i]/side;
         coY_C = corpus[i+Nq]/side;
         coZ_C = corpus[i+2*Nq]/side;
-        int boxC = coX_C + coY_C*d + coZ_C*d*d;
+        int boxC = coX_C + coY_C*d + coZ_C*d*d;     // Find the corresponding box of the point
         if ((boxC >= d*d*d) || (boxC < 0)){
             printf("Error, non existing box\n");
             exit(1);
         }
-        boxesC[boxC*elements + auxC[boxC]] = corpus[i];
+        boxesC[boxC*elements + auxC[boxC]] = corpus[i];     // --> Store the point inside the box 
         boxesC[boxC*elements + auxC[boxC] + newNq] = corpus[i + Nq];
-        boxesC[boxC*elements + auxC[boxC] + 2*newNq] = corpus[i + 2*Nq];
+        boxesC[boxC*elements + auxC[boxC] + 2*newNq] = corpus[i + 2*Nq];  // <--
         auxC[boxC]++;
         if (auxC[boxC]>=elements){
             printf("We have problem\n");
@@ -282,7 +292,7 @@ int main(int argc, char** argv){
     }
    
 
-    
+
     cudaFree(boxesC);
     cudaFree(auxC);
     cudaFree(queries);
